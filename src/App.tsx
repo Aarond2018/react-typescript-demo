cbc4ba9b8ac24556da288bc281446a327e63a7e8
Aarond2018 <aarondamilola1998@gmail.com>
2022-07-12 15:04:34 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, FormEvent, useEffect } from "react";
+import { TodoComponent } from "./Todo";
+import "./App.css";
+
+export type Todo = {
+	id: number;
+	todo: string;
+	isDone: boolean;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = useState<Todo[]>([]);
+  const [todo, setTodo] = useState<string>("")
+
+	const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+	const handleAdd = (event: FormEvent) => {
+		event.preventDefault();
+    if(inputRef.current?.value.length === 0) return;
+
+		setTodos((prevTodos) => [
+			...prevTodos,
+			{
+				id: Date.now(),
+				todo: `${inputRef.current?.value}`,
+				isDone: false,
+			},
+		]);
+    setTodo("")
+	};
+
+	const deleteTodo = (id: number) => {
+		setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+	};
+
+	const toggleDone = (id: number) => {
+		setTodos((prevTodos) =>
+			prevTodos.map((todo) =>
+				todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+			)
+		);
+	};
+
+	return (
+		<div className="App">
+			<h1>React-TypeScript To-do list</h1>
+			<form onSubmit={handleAdd}>
+				<input type="text" ref={inputRef} onChange={(e) => setTodo(e.target.value)} value={todo} />
+				<button type="submit">Add New</button>
+			</form>
+			<ul className="todos">
+				{todos.map((todo, key) => (
+					<TodoComponent
+						key={key}
+						id={todo.id}
+						todo={todo.todo}
+						isDone={todo.isDone}
+						deleteTodo={deleteTodo}
+						toggleDone={toggleDone}
+					/>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
